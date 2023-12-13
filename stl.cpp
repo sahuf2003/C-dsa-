46fbbc4e6591a130186c2a538098d4d654a15c33
sahuf2003 <127684377+sahuf2003@users.noreply.github.com>
2023-12-13 18:36:03 +0530

    Update and rename stl to stl.cpp
    vectors add vectors operation

---
+++ stl.cpp
@@ -0,0 +1,134 @@
+//started using stl(standard template library)
+//pair it stores 2 values;
+#include <iostream>
+using namespace std;
+int main() {
+    pair<int , int> p ={5,4};
+    cout<< p.first<< " " << p.second<< " ";
+    //nested pair
+    pair<pair<int ,int> , int> nest = {{4,6},5};
+    cout << nest.first.second <<" ";
+    cout<< nest.first.first << " ";
+    cout << nest.second<< " ";
+    //array in pair
+    pair<int , int> arr[] = {{3,4},{4,6}};
+    cout<< arr[1].first;
+    return 0;
+}
+
+// Vector is used for dynamic array
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> v;
+    v.push_back(9);
+    v.emplace_back(8);
+    cout<<v[0]<<" "<<v[1];
+    cout<<endl;
+    
+    //vector for pairs
+    vector<pair<int, int>> v2;
+    v2.push_back({3,4});
+    v2.emplace_back(6,6);
+    
+    cout<<v2[0].first<<" "<<v2[1].second;
+    cout<<endl;
+    //declaring vector
+    
+    vector<int> v1(5,200);
+        cout<<v1[0]<<" "; 
+        cout<<v1[1]<<" ";   
+        cout<<v1[2]<<" ";   
+        cout<<v1[3]<<" ";   
+        cout<<v1[4]<<" ";   
+        
+        cout<<endl;
+            vector<int> v3(5);
+        cout<<v3[0]<<" "; 
+        cout<<v3[1]<<" ";   
+        cout<<v3[2]<<" ";   
+        cout<<v3[3]<<" ";   
+        cout<<v3[4]<<" ";   
+        
+        cout<<endl;
+            vector<int> v5(v3);
+        cout<<v3[0]<<" "; 
+        cout<<v3[1]<<" ";   
+        cout<<v3[2]<<" ";   
+        cout<<v3[3]<<" ";   
+        cout<<v3[4]<<" ";   
+        cout<<endl;
+        
+        cout<<v1.back(); 
+        cout<<v.back();
+        
+        cout<<endl;
+        
+        vector<int>::iterator me = v.begin();
+        me++;
+        cout<<*(me)<<" ";
+        cout<<endl;
+        for(vector<int>::iterator us =v3.begin(); us!=v3.end();us++){
+            cout<<*(us)<< " ";
+        }
+        cout<<endl;
+        for(auto li=v.begin(); li!=v.end();li++){
+            cout<<*(li)<<" ";
+        }
+        cout<<endl; 
+        //for each loop
+        for(auto us :v){
+            cout<<us<<" ";
+        }
+        
+        
+        v.erase(v.begin()+1);//single element deletion
+        v3.erase(v.begin()+1, v3.begin() +5); //(start, end) elements in between will be deleted
+        vector<int> del(5);
+        del[0]= 10;
+        del[1]= 3;
+        del[2] = 4;
+        del[3] =7;
+        del[4] =8;
+        cout <<endl;
+        for(auto lol: del){
+            cout<<lol<<" ";
+        }
+                del.erase(del.begin());
+        cout<<endl;
+        for(auto lol: del){
+            cout<<lol<<" ";
+        }
+        cout<<endl;
+        del.erase(del.begin()+1,del.begin()+3);
+                for(auto lol: del){
+            cout<<lol<<" ";
+        }
+        cout<<endl;
+        
+        del.insert(del.begin()+1,78);
+        del.insert(del.begin(),9,10);//insert 10 9 times
+        del.insert(del.begin(),v1.begin(),v1.end());//insert whole error
+                for(auto lol: del){
+            cout<<lol<<" ";
+        }
+        cout<<endl;
+        cout<<del.size();//size check
+        del.pop_back();//last element ko pop karayga
+        for(auto lol: del){
+        cout<<lol<<" ";
+        }
+        cout<<endl;
+        del.swap(v1);//swap karega obviously
+                        for(auto lol: del){
+            cout<<lol<<" ";
+        }
+        cout<<endl;
+                        for(auto lol: v1){
+            cout<<lol<<" ";
+        }
+        del.clear();//khali karega
+        cout<<del.empty();//check karega khali hai kya?
+    return 0;
+}
