b145aaf3bd39e072415aac95601f77c0419c789e
sahuf2003 <127684377+sahuf2003@users.noreply.github.com>
2023-12-15 19:07:31 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -28,3 +28,102 @@ vector<int> printNos(int x) {
     return v;
 }
 //recursion stackoverflow base case recursrion tree
+//printing name 5 time
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int cnt = 0;
+void name(){
+    cout<<"hello sahuf"<<endl;
+    if(cnt==4) return;
+    cnt++;
+    name();
+}
+
+
+int main() {
+    // Write C++ code here
+    cout << "Hello world!";
+    name();
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+
+void print(int i,int n){
+    if(i>n) return;
+    cout<<"Hello Sahuf"<<endl;
+    print(i+1,n);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<n<<endl;
+    print(1,n);
+}
+
+//print 1 to n with recursion
+#include<iostream>
+using namespace std;
+
+void print(int i,int n){
+    if(i>n) return;
+    cout<<i<<endl;
+    print(i+1,n);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print(1,n);
+}
+
+//print n to 1
+
+#include<iostream>
+using namespace std;
+
+void print(int i,int n){
+    if(i<1) return;
+    cout<<i<<endl;
+    print(i-1,n);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print(n,n);
+}
+
+//print 1 to n with backtracking
+#include<iostream>
+using namespace std;
+
+void print(int i,int n){
+    if(i<1) return;
+    print(i-1,n);
+    cout<<i<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print(n,n);
+}
+//print n to 1 with backtracing
+#include<iostream>
+using namespace std;
+
+void print(int i,int n){
+    if(i>n) return;
+    print(i+1,n);
+    cout<<i<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print(1,n);
+}
